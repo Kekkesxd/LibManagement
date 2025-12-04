a63ad12e9662b15fd19e888c57c5c0783296d8a4
Fares Abualkheir <Fares.Abuk@gmail.com>
2025-12-04 19:28:18 +0300

    fixed repo issue and wrote some of member class

--- LibManagement/src/Member.java
+++ LibManagement/src/Member.java
@@ -1,2 +1,40 @@
+
+
 public class Member {
+    private String name;
+    private String memID;
+    private String email;
+    private int number;
+    private int booksborrowed;
+    private int maxBooksAllowed;
+
+    public Member(){
+        this.maxBooksAllowed = 2;
+    }
+
+    //Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setMemID(String memID){
+        this.memID = memID;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public void setBooksborrowed(int booksborrowed){
+        this.booksborrowed = booksborrowed;
+    }
+
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public String getMemID(){
+        return memID;
+    }
+    
 }
